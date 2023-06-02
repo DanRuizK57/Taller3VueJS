bdafdd5b2cd77a40a18cba6cc6f1a5f9eea44770
GabeASM <g.aillapan01@ufromail.cl>
2023-06-02 15:56:57 -0400

    avance componente

--- src/services/marketplace_api.services.js
+++ src/services/marketplace_api.services.js
@@ -1,3 +1,12 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://ec2-54-163-208-73.compute-1.amazonaws.com:8080';
+
+
+function listarProductos(){
+    return axios.get(`${BASE_URL}/products`).then((result)=>{
+        return result.data
+    })
+}
+
+export {listarProductos}
