732b889530c8940d52beca8f74119052dedce527
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-06-02 16:44:16 -0400

    Formato página terminado

--- src/views/Reseñas.vue
+++ src/views/Reseñas.vue
@@ -2,22 +2,22 @@
   <h1>Reseñas</h1>
   <reseña-item></reseña-item>
   <div v-if="usuario">
-    <img :src="usuario.photo" alt="Forto de usuario">
+    <img :src="usuario.photo" alt="Forto de usuario" id="fotoPerfil">
     <h2>{{usuario.name}}</h2>
     <h4>{{usuario.city}}</h4>
     <p>{{usuario.bio}}</p>
     <h3>Reseñas creadas por {{ usuario.name }} ({{reseñas.length}})</h3>
   </div>
-  <div>
+  <div v-for="reseña in reseñas" :key="reseña.id" class="reseñas">
     <div class="card mb-3" style="max-width: 540px;">
       <div class="row g-0">
         <div class="col-md-4">
-          <img src="" class="img-fluid rounded-start" alt="">
+          <img :src="reseña.product.images[0]" class="img-fluid rounded-start" alt="">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">product.name</h5>
-            <p class="card-text">product.description</p>
+            <h5 class="card-title">{{ reseña.product.name }}</h5>
+            <p class="card-text">{{reseña.product.description}}</p>
           </div>
         </div>
       </div>
@@ -50,5 +50,17 @@ export default {
 </script>
 
 <style>
-
+#fotoPerfil {
+    height: 300px;
+    widows: 300px;
+    border: 1px solid black;
+    border-radius: 1000px;
+    margin-top: 5%;
+    margin-bottom: 2%;
+}
+.reseñas {
+    margin-top: 5%;
+    display: flex;
+    justify-content: center;
+}
 </style>
