8742f22bed7959bc34fb89046fa7b0417001ce57
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-06-02 20:50:10 -0400

    Componente ReseñaItem añadido a Reseñas.vue

--- src/views/Reseñas.vue
+++ src/views/Reseñas.vue
@@ -1,46 +1,44 @@
 <template>
   <div v-if="usuario">
     <img :src="usuario.photo" alt="Forto de usuario" id="fotoPerfil">
-    <h2>{{usuario.name}}</h2>
-    <h4>{{usuario.city}}</h4>
-    <p>{{usuario.bio}}</p>
-    <h3>Reseñas creadas por {{ usuario.name }} ({{reseñas.length}})</h3>
+    <h2>{{ usuario.name }}</h2>
+    <h4>{{ usuario.city }}</h4>
+    <p>{{ usuario.bio }}</p>
+    <h3>Reseñas creadas por {{ usuario.name }} ({{ reseñas.length}})</h3>
   </div>
-  <div v-for="reseña in reseñas" :key="reseña.id" class="reseñas">
-    <div class="card mb-3" style="max-width: 540px;">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img :src="reseña.product.images[0]" class="img-fluid rounded-start" alt="">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">{{ reseña.product.name }}</h5>
-            <p class="card-text">{{reseña.review}}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div v-for="(reseña, index) in reseñas" :key="index" class="reseñas">
+    <ReseñaItem 
+    :img="reseña.product.images[0]"
+    :name="reseña.product.name"
+    :descripcion="reseña.review">
+      </ReseñaItem>
+    
   </div>
 </template>
 
 <script>
 import { obtenerResenas } from "../services/marketplace_api.services";
+import ReseñaItem from '../components/ReseñaItem.vue';
 
 export default {
     name: "ReseñasPage",
+    components: {
+      ReseñaItem,
+    },
     data() {
         return {
             reseñas: [],
-            usuario: null,
+            usuario: Object,
             usuarioId: "",
         };
     },
     async mounted() {
         this.usuarioId = this.$route.params.userid;
         const result = await obtenerResenas(this.usuarioId);
-        this.usuario = result.user;
+
         this.reseñas = result.reviews;
-        console.log(result)
+        this.usuario = result.user;
+
     },
 };
 </script>
